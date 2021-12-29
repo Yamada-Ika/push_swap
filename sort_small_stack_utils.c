eea51fd3786df1bce0c21ce3c1f08818caf1ba9b
Yamada-Ika <73375669+Yamada-Ika@users.noreply.github.com>
2021-12-29 18:11:29 +0900

    [Fix]

--- sort_small_stack_utils.c
+++ sort_small_stack_utils.c
@@ -6,24 +6,24 @@
 /*   By: iyamada <iyamada@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/28 20:10:40 by iyamada           #+#    #+#             */
-/*   Updated: 2021/12/28 22:59:05 by iyamada          ###   ########.fr       */
+/*   Updated: 2021/12/29 12:26:26 by iyamada          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "order.h"
 
-static void ft_sort_three_helper(t_stack *a, int vals[3])
+static void	ft_sort_three_helper(t_stack *a, int bottm, int centr, int top)
 {
-	if (vals[2] > vals[1] && vals[1] < vals[0] && vals[2] < vals[0])
+	if (top > centr && centr < bottm && top < bottm)
 		ft_sa(a);
-	else if (vals[2] > vals[1] && vals[1] > vals[0])
+	else if (top > centr && centr > bottm)
 	{
 		ft_sa(a);
 		ft_rra(a);
 	}
-	else if (vals[2] > vals[1] && vals[1] < vals[0] && vals[2] > vals[0])
+	else if (top > centr && centr < bottm && top > bottm)
 		ft_ra(a);
-	else if (vals[2] < vals[1] && vals[1] > vals[0] && vals[2] < vals[0])
+	else if (top < centr && centr > bottm && top < bottm)
 	{
 		ft_sa(a);
 		ft_ra(a);
@@ -34,30 +34,16 @@ static void ft_sort_three_helper(t_stack *a, int vals[3])
 
 void	ft_sort_three(t_stack *a)
 {
-	int	vals[3];
+	int	bottm;
+	int	centr;
+	int	top;
 
 	if (ft_is_sorted(a))
 		return ;
-	vals[0] = a->front->val;
-	vals[1] = a->front->front->val;
-	vals[2] = a->back->val;
-	// ft_sort_three_helper(a, vals);
-	if (vals[2] > vals[1] && vals[1] < vals[0] && vals[2] < vals[0])
-		ft_sa(a);
-	else if (vals[2] > vals[1] && vals[1] > vals[0])
-	{
-		ft_sa(a);
-		ft_rra(a);
-	}
-	else if (vals[2] > vals[1] && vals[1] < vals[0] && vals[2] > vals[0])
-		ft_ra(a);
-	else if (vals[2] < vals[1] && vals[1] > vals[0] && vals[2] < vals[0])
-	{
-		ft_sa(a);
-		ft_ra(a);
-	}
-	else
-		ft_rra(a);
+	bottm = a->front->val;
+	centr = a->front->front->val;
+	top = a->back->val;
+	ft_sort_three_helper(a, bottm, centr, top);
 }
 
 void	ft_sort_under_six(t_stack *a, t_stack *b, int a_size)
--- stack_utils.h
+++ stack_utils.h
@@ -6,19 +6,18 @@
 /*   By: iyamada <iyamada@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/26 16:12:44 by iyamada           #+#    #+#             */
-/*   Updated: 2021/12/26 16:14:03 by iyamada          ###   ########.fr       */
+/*   Updated: 2021/12/29 12:26:45 by iyamada          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef STACK_UTILS_H
 # define STACK_UTILS_H
 
-#include "bilist.h"
-#include "push_swap.h"
+# include "bilist.h"
+# include "push_swap.h"
 
 t_stack	*ft_new_stack(int	*arry, size_t size);
 int		ft_get_stack_size(t_stack *stack);
 void	ft_delete_stack(t_stack *stack);
 
 #endif
-
