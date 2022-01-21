8e3e67f085c608a4201ff8a548fd3829034149b1
Yamada-Ika <73375669+Yamada-Ika@users.noreply.github.com>
2022-01-22 00:40:52 +0900

    fix

--- push_swap_files/include/push_swap.h
+++ push_swap_files/include/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: iyamada <iyamada@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/20 11:19:03 by iyamada           #+#    #+#             */
-/*   Updated: 2022/01/22 00:24:48 by iyamada          ###   ########.fr       */
+/*   Updated: 2022/01/22 00:33:25 by iyamada          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,14 +20,14 @@
 
 # include <stdbool.h>
 
-// ft_is_wrong_arry.c
+// utils/ft_is_wrong_arry.c
 bool	ft_is_wrong_arry(char *argv[]);
 
 // stack
 t_stack	*ft_new_stack(int	*arry, size_t size);
 void	ft_delete_stack(t_stack *stack);
 
-// ft_get_arry_from_arg.c
+// utils/ft_get_arry_from_arg.c
 int		*ft_get_arry_from_arg(char *argv[]);
 void	ft_arry_compress(int *arry, int size);
 
