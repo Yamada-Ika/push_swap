2f98197e6c761e719b9c9b52ee95c30381b99124
Yamada-Ika <73375669+Yamada-Ika@users.noreply.github.com>
2022-01-22 00:29:18 +0900

    fix:

---
+++ utils/ft_get_arry_from_arg.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_get_arry_from_arg.c                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: iyamada <iyamada@student.42tokyo.jp>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/21 23:31:20 by iyamada           #+#    #+#             */
+/*   Updated: 2022/01/22 00:25:11 by iyamada          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "utils.h"
+
+int	*ft_get_arry_from_arg(char **argv)
+{
+	int			*arry;
+	char		**strs;
+	size_t		i;
+	size_t		j;
+	size_t		k;
+
+	arry = (int *)malloc(ft_get_size(argv) * sizeof(int));
+	if (arry == NULL)
+		return (NULL);
+	i = 0;
+	j = 1;
+	while (argv[j] != NULL)
+	{
+		strs = ft_split2(argv[j], ' ');
+		k = 0;
+		while (strs[k] != NULL)
+		{
+			arry[i] = ft_atoi(strs[k]);
+			i++;
+			k++;
+		}
+		ft_frees(strs);
+		j++;
+	}
+	return (arry);
+}
